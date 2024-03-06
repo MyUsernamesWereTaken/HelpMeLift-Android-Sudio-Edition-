94699692c4f7a3bbe3a9cf73d24d7843888b55a6
MyUsernamesWereTaken <s.parvez@se21.qmul.ac.uk>
2024-03-06 00:25:17 +0000

    Hiding and Viewing workout selections based on muscle selection to be completed. All XML variables set and using if statements in the java code to control which ones are being activated and which fragment should be invisible.
    Need to add onto the XML and add FrameLayouts into (activity_muscle.exercises.xml).

--- app/src/main/java/com/example/helpmelift/MuscleExercises.java
+++ app/src/main/java/com/example/helpmelift/MuscleExercises.java
@@ -6,10 +6,12 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.helpmelift.databinding.ActivityMuscleExercisesBinding;
+import com.example.helpmelift.ui.MuscleGroups.MuscleGroupsFragment;
 
 public class MuscleExercises extends AppCompatActivity {
 
     private ActivityMuscleExercisesBinding binding;
+    private String muscleSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +19,179 @@ public class MuscleExercises extends AppCompatActivity {
         setContentView(R.layout.activity_muscle_exercises);
 
         String muscleSelection = getIntent().getStringExtra("Muscle");
+        this.muscleSelected = muscleSelection;
         ((TextView)findViewById(R.id.muscleSelected)).setText(muscleSelection);
+
+        if(this.muscleSelected.equals("Deltoids")){
+            binding.setDeltsChosen(true);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Abs")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(true);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Biceps")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(true);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Calves")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(true);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Glutes")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(true);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Forearms")) {
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(true);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Chest")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(true);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Triceps")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(true);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Hamstring")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(true);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Quadriceps")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(true);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Trapezius")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(true);
+            binding.setLatsChosen(false);
+        }else if(this.muscleSelected.equals("Latissimus Dorsi")){
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(true);
+        }else{
+            binding.setDeltsChosen(false);
+            binding.setAbsChosen(false);
+            binding.setBicepsChosen(false);
+            binding.setCalvesChosen(false);
+            binding.setGlutesChosen(false);
+            binding.setForearmsChosen(false);
+            binding.setChestChosen(false);
+            binding.setTricepsChosen(false);
+            binding.setHamstringsChosen(false);
+            binding.setQuadsChosen(false);
+            binding.setTrapsChosen(false);
+            binding.setLatsChosen(false);
+        }
     }
 
 
